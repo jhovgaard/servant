941ed4f26394344b456098fe9ea99c79209a3233
Jonas Hovgaard <j@jhovgaard.dk>
2012-12-16 02:01:49 +0100

    Fixed #21

---
+++ src/Servant.Business/Objects/Binding.cs
@@ -0,0 +1,22 @@
+﻿using Servant.Business.Objects.Enums;
+
+namespace Servant.Business.Objects
+{
+    public class Binding
+    {
+        public Protocol Protocol { get; set; }
+        public string Hostname { get; set; }
+        public int Port { get; set; }
+        public string CertificateName { get; set; }
+        public byte[] CertificateHash { get; set; }
+        public new string ToString()
+        {
+            return Protocol.ToString() + "://" + (string.IsNullOrWhiteSpace(Hostname) ? "*" : Hostname) + ":" + Port;
+        }
+
+        public string ToIisBindingInformation()
+        {
+            return string.Format("*:{0}:{1}", Port, Hostname);
+        }
+    }
+}
