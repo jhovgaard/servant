f7ced9e57a47e8be96d64dc0ef03565e081e09d2
Jonas Hovgaard <j@jhovgaard.dk>
2013-07-05 22:04:39 +0200

    Fixed broke tests caused by Nancy upgrade

---
+++ src/Servant.Web.Tests/Helpers/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using Nancy.Testing.Fakes;
+
+namespace Servant.Web.Tests.Helpers
+{
+    public class Bootstrapper : Server.Bootstrapper
+    {
+        // Using this because http://stackoverflow.com/questions/15138994/sequence-contains-more-than-one-element-an-nancybootstrapperbase-class
+        protected override IRootPathProvider RootPathProvider
+        {
+            get
+            {
+                return new FakeRootPathProvider();
+            }
+        }
+    }
+}
