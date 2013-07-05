5453ad9aa97f8d5f0c87dd4408f74cb6347f7487
Jonas Hovgaard <j@jhovgaard.dk>
2013-07-05 22:17:39 +0200

    Updated chocolatey package

--- Chocolatey Template/tools/chocolateyUninstall.ps1
+++ Chocolatey Template/tools/chocolateyUninstall.ps1
@@ -1,4 +1,3 @@
 ﻿$packageName = 'Servant' # arbitrary name for the package, used in messages
-$url = 'http://storage.servant.io/files/servant-1.0.5.zip' # download url
 $servantExe = Join-Path "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" 'servant.server.exe'
 Start-ChocolateyProcessAsAdmin "uninstall" $servantExe -minimized
