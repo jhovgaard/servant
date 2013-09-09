7de7cbbed96460f96391143897839d45086bfb93
Jonas Hovgaard <j@jhovgaard.dk>
2013-09-09 22:15:55 +0200

    Removed unused classes

--- src/Servant.Web/Modules/StatsModule.cs
+++ src/Servant.Web/Modules/StatsModule.cs
@@ -8,23 +8,6 @@ namespace Servant.Web.Modules
     {
         public StatsModule() : base("/stats/")
         {
-            Get["/"] = p => {
-                var serverStats = new Business.Objects.Reporting.ServerStats();
-                
-                //serverStats.TotalRequests = logEntryService.GetTotalCount();
-                serverStats.DataRecieved = "Not available";
-                serverStats.DataSent = "Not available";
-                serverStats.TotalSites = SiteManager.GetSites().Count();
-
-
-                //serverStats.AverageResponeTime = (int)logEntryService.GetAverageResponseTime();
-
-                //serverStats.TotalErrors = applicationErrorService.GetTotalCount();
-                serverStats.UnusedApplicationPools = ApplicationPoolHelper.GetUnusedApplicationPools().Count();
-                Model.ServerStats = serverStats;
-                return View["Index", Model];
-            };
-
             Get["/cleanupapplicationpools/"] = p => {
                 Model.UnusedApplicationPools = ApplicationPoolHelper.GetUnusedApplicationPools();
                 return View["CleanupApplicationPools", Model];
