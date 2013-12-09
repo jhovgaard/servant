4783b6f9d0388e4f585487fda9b0651bc62e4380
Kordonme <mark@kordon.me>
2013-12-09 20:51:59 +0100

    Added simple API support for Servant
    - Machine and system data
    - List of sites
    - Simple actions for sites (stop, start, restart and recycle)

---
+++ src/Servant.Web/Modules/ApiModule.cs
@@ -0,0 +1,118 @@
+﻿using System.IO;
+using System.Linq;
+using Nancy;
+using Servant.Business.Objects;
+using Servant.Business.Objects.Enums;
+using Servant.Web.Helpers;
+using Servant.Web.Performance;
+
+namespace Servant.Web.Modules
+{
+    public class ApiModule : BaseModule
+    {
+        public ApiModule()
+            : base("/api/")
+        {
+            var configuration = Nancy.TinyIoc.TinyIoCContainer.Current.Resolve<ServantConfiguration>();
+
+            Before += ctx => !configuration.EnableApi ? new NotFoundResponse() : null;
+
+            Get["/"] = p => "Servant API";
+
+            #region Stats
+            Get["/stats/"] = p =>
+            {
+                var sites = SiteManager.GetSites().ToList();
+                var drives = DriveInfo.GetDrives().Where(x => x.DriveType == DriveType.Fixed).Select(
+                        x => new { x.Name, x.TotalSize, x.AvailableFreeSpace });
+
+                return Response.AsJson(new
+                {
+                    System.Environment.MachineName,
+                    PerformanceData.SystemUpTime,
+                    PerformanceData.TotalMemory,
+                    PerformanceData.PhysicalAvailableMemory,
+                    PerformanceData.CpuUsage,
+                    PerformanceData.TotalGetRequestsSec,
+                    Drives = drives,
+                    Sites = sites.Count(),
+                    SitesStopped = sites.Count(x => x.SiteState != InstanceState.Started)
+                });
+            };
+            #endregion
+
+            #region Sites
+            Get["/sites/{id?}/"] = p =>
+            {
+                var id = (int?)p.id;
+
+                if (id.HasValue)
+                {
+                    var site = SiteManager.GetSiteById(id.Value);
+
+                    return site == null ? new NotFoundResponse() : Response.AsJson(site);
+                }
+
+                var sites = SiteManager.GetSites();
+                return Response.AsJson(sites);
+            };
+
+            Post["/sites/{id}/stop/"] = p =>
+            {
+                var id = (int?)p.id;
+
+                if (!id.HasValue)
+                {
+                    return new NotFoundResponse();
+                }
+
+                var site = SiteManager.GetSiteById((int)p.id);
+                SiteManager.StopSite(site);
+                return Response.AsJson(site);
+            };
+
+            Post["/sites/{id}/start/"] = p =>
+            {
+                var id = (int?)p.id;
+
+                if (!id.HasValue)
+                {
+                    return new NotFoundResponse();
+                }
+
+                var site = SiteManager.GetSiteById((int)p.id);
+                SiteManager.StartSite(site);
+                return Response.AsJson(site);
+            };
+
+            Post["/sites/{id}/restart/"] = p =>
+            {
+                var id = (int?)p.id;
+
+                if (!id.HasValue)
+                {
+                    return new NotFoundResponse();
+                }
+
+                Site site = SiteManager.GetSiteById(p.Id);
+                SiteManager.RestartSite(site.IisId);
+                return Response.AsJson(site);
+            };
+
+            Post["/sites/{id}/recycle/"] = p =>
+            {
+                var id = (int?)p.id;
+
+                if (!id.HasValue)
+                {
+                    return new NotFoundResponse();
+                }
+
+                Site site = SiteManager.GetSiteById(p.Id);
+                SiteManager.RecycleApplicationPoolBySite(site.IisId);
+                return Response.AsJson(site);
+            };
+            #endregion
+        }
+    }
+}
---
+++ src/Servant.Web/Performance/SystemInfoWrapper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Servant.Web.Performance
+{
+    public static class SystemInfoWrapper
+    {
+        [DllImport("psapi.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetPerformanceInfo([Out] out PerformanceInformation performanceInformation, [In] int size);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PerformanceInformation
+        {
+            public int Size;
+            public IntPtr CommitTotal;
+            public IntPtr CommitLimit;
+            public IntPtr CommitPeak;
+            public IntPtr PhysicalTotal;
+            public IntPtr PhysicalAvailable;
+            public IntPtr SystemCache;
+            public IntPtr KernelTotal;
+            public IntPtr KernelPaged;
+            public IntPtr KernelNonPaged;
+            public IntPtr PageSize;
+            public int HandlesCount;
+            public int ProcessCount;
+            public int ThreadCount;
+        }
+
+        public static Int64 GetPhysicalAvailableMemory()
+        {
+            var performanceInformation = new PerformanceInformation();
+
+            if (GetPerformanceInfo(out performanceInformation, Marshal.SizeOf(performanceInformation)))
+            {
+                return performanceInformation.PhysicalAvailable.ToInt64()*performanceInformation.PageSize.ToInt64();
+            }
+
+            return -1;
+        }
+
+        public static Int64 GetTotalMemory()
+        {
+            var performanceInformation = new PerformanceInformation();
+
+            if (GetPerformanceInfo(out performanceInformation, Marshal.SizeOf(performanceInformation)))
+            {
+                return performanceInformation.PhysicalTotal.ToInt64()*performanceInformation.PageSize.ToInt64();
+            }
+
+            return -1;
+        }
+    }
+}
