37c56d13f3b81a2e7132b51eec6fda9edebd6503
Jonas Hovgaard <j@jhovgaard.dk>
2014-09-25 17:45:45 +0200

    Updater now gets github releases

--- src/Servant.Updater/Program.cs
+++ src/Servant.Updater/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -13,17 +14,20 @@ namespace Servant.Updater
     {
         static void Main(string[] args)
         {
-            var path = args[0];
-            var servantZipUrl = args[1];
-            
-            //var path = @"C:\Code\servant\src\Servant.Server\bin\Release\Servant.Server.exe";
-            //var servantZipUrl = "https://github.com/jhovgaard/servant/releases/download/1.1/servant-1.1.zip";
+            //var path = args[0];
+            //var servantZipUrl = args[1];
+            var webClient = new WebClient() { };
+            var path = @"C:\Code\servant\src\Servant.Server\bin\Release\Servant.Server.exe";
             var servantDirectory = System.IO.Path.GetDirectoryName(path);
 
             var downloadLocation = System.IO.Path.Combine(servantDirectory, "servant-1.1.zip");
 
+            var releases = GetReleases();
+
+
+            
             Console.WriteLine("Downloading " + servantZipUrl + "...");
-            new WebClient().DownloadFile(servantZipUrl, downloadLocation);
+            webClient.DownloadFile(servantZipUrl, downloadLocation);
             var fastZip = new FastZip();
             
             Console.WriteLine("Uninstalling current version");
@@ -33,6 +37,27 @@ namespace Servant.Updater
             var installResult = Install(path);
             Console.WriteLine("Installing new version");
         }
+
+        private static List<GithubReleaseResult> GetReleases()
+        {
+            string lcUrl = "https://api.github.com/repos/jhovgaard/servant/releases";
+
+            // *** Establish the request
+            var request = (HttpWebRequest)WebRequest.Create(lcUrl);
+
+            // *** Set properties
+            request.UserAgent = "Servant for IIS Client";
+            request.KeepAlive = false;
+
+            // *** Retrieve request info headers
+            var loWebResponse = (HttpWebResponse)request.GetResponse();
+            var loResponseStream =new StreamReader(loWebResponse.GetResponseStream());
+            string result = loResponseStream.ReadToEnd();
+            loWebResponse.Close();
+            loResponseStream.Close();
+
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<List<GithubReleaseResult>>(result);
+        } 
 
         static string Uninstall(string path)
         {
