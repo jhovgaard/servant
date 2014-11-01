5c91b9f47d8e5e6e54ba88c8d851ac8ff583f21f
Kordonme <mark@kordon.me>
2014-11-01 19:31:45 +0100

    Return correct servant version

--- src/Servant.Client/Infrastructure/ServantClientConfiguration.cs
+++ src/Servant.Client/Infrastructure/ServantClientConfiguration.cs
@@ -11,7 +11,7 @@ namespace Servant.Client.Infrastructure
         public ServantClientConfiguration()
         {
             var version = Assembly.GetExecutingAssembly().GetName().Version;
-            Version = new ServantVersion(version.Major, version.Minor, version.Revision);
+            Version = new ServantVersion(version.Major, version.Minor, version.Build);
         }
 
         public Guid InstallationGuid { get; set; }
