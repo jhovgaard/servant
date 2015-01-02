efb250f796e84930ffe00055af8c72bc0c6b0b95
Jonas Hovgaard <j@jhovgaard.dk>
2015-01-02 23:39:51 +0100

    Elaborated chocolatey error

--- Chocolatey Template/tools/chocolateyInstall.ps1
+++ Chocolatey Template/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $url = "https://github.com/jhovgaard/servant/releases/download/1.0.14-client/Ser
 $validExitCodes = @(0,3010)
 $packageParameters = $env:chocolateyPackageParameters;
 if(!$packageParameters) {
-  Write-ChocolateyFailure "servant" "Missing key"
+  Write-ChocolateyFailure "servant" "Missing key. To install Servant you need to provide your key as the packageParameters.`r`nUsage example: cinst servant -packageParameters ""111111de-11b1-1111-b11b-11111d1a1db1"""
   return;
 }
 
